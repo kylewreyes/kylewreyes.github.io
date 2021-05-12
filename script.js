765f64338f366e87eab1976cd67224e79c513ec5
Kyle <kyle_reyes@brown.edu>
2021-05-12 01:00:28 -0400

    Minimized image size, fixed mobile home screen full size issue, fixed navbar highlighting bugs

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
+// TODO: desktop navbar disappears on hard refresh
+
+const MOBILE_NAVBAR_LIMIT = 800;
+
+function calculateMobileHt() {
+  // document.documentElement.clientHeight is more reliable than
+  // window.innerHeight when it comes to mobile browsers
+  document
+    .querySelector(":root")
+    .style.setProperty(
+      "--vh",
+      document.documentElement.clientHeight / 100 + "px"
+    );
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  calculateMobileHt();
+  window.addEventListener("resize", calculateMobileHt);
+});
+
 /**
  * Need to ensure that all the elements are fully loaded before calculating the
  * offsets.
  */
 window.addEventListener("load", function () {
+  const navBar = document.getElementById("navbar");
+  const navBarLinks = document.getElementsByClassName("navbar-link");
+
   window.addEventListener("scroll", updateNavBar);
   // When the viewport is resized, need to recalculate the values at which the
   // active section becomes highlighted
   window.addEventListener("resize", calcSectionOffets);
+  window.addEventListener("resize", updateNavBar);
+  // When resizing, need to ensure that the navbar switches appropriately
+  // (through the media queries), but that requires removing the styling from
+  // the JS, which takes higher precedence
+  window.addEventListener("resize", function () {
+    navBar.style.display = null;
+  });
 
-  const navBar = document.getElementById("navbar");
-  const navBarLinks = document.getElementsByClassName("navbar-link");
   let sectionOffsets = [];
 
   function calcSectionOffets() {
     sectionOffsets = [];
     for (let section of document.getElementsByTagName("section")) {
-      sectionOffsets.push(section.offsetTop);
+      const val =
+        window.pageYOffset + Math.floor(section.getBoundingClientRect().top);
+      sectionOffsets.push(val);
     }
 
-    const MOBILE_NAVBAR_LIMIT = 800;
     const MOBILE_NAVBAR_HT = 50;
     if (window.innerWidth <= MOBILE_NAVBAR_LIMIT) {
       for (i = 1; i < sectionOffsets.length; ++i) {
@@ -28,6 +57,8 @@ window.addEventListener("load", function () {
   }
 
   calcSectionOffets();
+  console.log(sectionOffsets);
+  updateNavBar();
 
   const navBarButton = document.getElementById("navbar-button");
   navBarButton.addEventListener("click", toggleNavBar);
@@ -38,11 +69,20 @@ window.addEventListener("load", function () {
    */
   function updateNavBar() {
     // Changes from absolute to fixed position for desktop
-    const arrowDownPos = document.getElementById("arrow-box").offsetTop;
-    if (window.pageYOffset >= arrowDownPos) {
-      navBar.classList.add("sticky");
+    if (window.innerWidth <= MOBILE_NAVBAR_LIMIT) {
+      const arrowDownPos = document.getElementById("navbar-header").offsetTop;
+      if (window.pageYOffset >= arrowDownPos) {
+        navBar.classList.add("sticky");
+      } else {
+        navBar.classList.remove("sticky");
+      }
     } else {
-      navBar.classList.remove("sticky");
+      const arrowDownPos = document.getElementById("arrow-box").offsetTop;
+      if (window.pageYOffset >= arrowDownPos) {
+        navBar.classList.add("sticky");
+      } else {
+        navBar.classList.remove("sticky");
+      }
     }
 
     for (link of navBarLinks) {
